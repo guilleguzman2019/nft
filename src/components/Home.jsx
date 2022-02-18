51f3ff580b2306d76bfaebce8d4b7c4739435039
guilleguzman <guillermoguzman.2016@gmail.com>
2022-02-18 04:24:54 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import Web3 from 'web3/dist/web3.min.js'
 import { CONTACT_ABI } from '../abi/abi.js';
 
 
-const contractAddress = '0x39898C3415266c0a347d86bB9d8E2a9fb1eF68ec';
+const contractAddress = '0x3a95877a12953e1B703F7f3f0b4df752C7734F36';
 
 const web3 = new Web3(window.ethereum);
 
