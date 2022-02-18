a182866ccbcd301aaf5ab2e9614191ba35c85793
guilleguzman <guillermoguzman.2016@gmail.com>
2022-02-18 05:28:30 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -44,7 +44,8 @@ function Home() {
 
       <div class="container">
         <div class="row justify-content-center">
-          <h3>Total minteado hasta el momento {totalMinted}/20000</h3>
+          <h4 class="titulo">Total minteado hasta el momento {totalMinted}/20000</h4>
+          <br>
           <h1 class="titulo">Pretty Funckies Token final</h1>
         </div>
       </div>
